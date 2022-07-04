b76862805df620bb3f585dc80670bd20dd9b5169
XLanLi <xlanli0321@gmail.com>
2022-07-04 21:32:03 +0100

    NLU, TTS, Animation in Update
    with NLU issues

--- unity/ARView-IBM-TextMessaging/Assets/main_test.cs
+++ unity/ARView-IBM-TextMessaging/Assets/main_test.cs
@@ -18,6 +18,12 @@ public class main_test : MonoBehaviour
     public TMP_InputField messageInput;
     public Button confirmActionButton;
 
+    private watsonNLUTTSIF watsonService;
+    private bool isPlaying_lastframe = false;
+    private string message;
+    private int messageSignal;
+    private int emotionSignal = 0;
+    private ARObjectControl curARObjControl;
 
     private void Awake()
     {
@@ -26,12 +32,57 @@ public class main_test : MonoBehaviour
 
     void Start()
     {
-
+        curARObjControl = objectToPlace.GetComponent<ARObjectControl>();
     }
 
     void Update()
     {
-
+        if(messageSignal==1)
+        {
+            if(!string.IsNullOrEmpty(message)) {
+                watsonService = new watsonNLUTTSIF(message);
+                //watsonService.soundTTS();
+                message = string.Empty;
+            }
+            // NLU TTS    
+            if(watsonService.getNLUResult()!=null) {
+                emotionSignal = watsonService.getEmotion();
+                Debug.Log(emotionSignal);
+                watsonService.soundTTS();
+            }  
+            //Animate
+            if (watsonService.getStatusOfAudio() && (!isPlaying_lastframe)) {
+                if(emotionSignal==1) {
+                    curARObjControl.Animator.SetTrigger("sadness");
+                }
+                else if(emotionSignal==2) {
+                    curARObjControl.Animator.SetTrigger("joy");
+                }
+                else if(emotionSignal==3) {
+                    curARObjControl.Animator.SetTrigger("fear");
+                }
+                else if(emotionSignal==4) {
+                    curARObjControl.Animator.SetTrigger("disgust");
+                }
+                else if(emotionSignal==5) {
+                    curARObjControl.Animator.SetTrigger("anger");
+                }
+                else {
+                    curARObjControl.Animator.SetTrigger("normal");
+                }
+                isPlaying_lastframe = true;
+            }
+            else if ((!watsonService.getStatusOfAudio()) && isPlaying_lastframe) {
+                curARObjControl.Animator.SetTrigger("normal");
+                isPlaying_lastframe = false;
+            }
+            
+        }
+        else if (messageSignal==0)
+        {
+            watsonService.destroyAudio();
+            curARObjControl.Animator.SetTrigger("normal");
+        }
 
     }
 
@@ -39,38 +90,10 @@ public class main_test : MonoBehaviour
     // Actions after pressing confirm button
     private void OnConfirmAction()
     {
-        string message = messageInput.text;
-        if(string.IsNullOrEmpty(message)) {
-            return;
-        }
-        //NLU TTS
-        watsonNLUTTSIF watsonService = new watsonNLUTTSIF(message);
-        //Animation
-        int emotionSignal = -1;
-        Debug.Log("getNLUResult  " + watsonService.getNLUResult());
-        /*while (watsonService.getNLUResult() == null)
-        {
-            
-        }*/
-        emotionSignal = watsonService.getEmotion();
-        Debug.Log("emotionSignal  " + emotionSignal);
-        ARObjectControl curARObjControl = objectToPlace.GetComponent<ARObjectControl>();
-        Animator ani = curARObjControl.Animator;
-        
-        if(emotionSignal==1) {
-            ani.SetTrigger("sadness");
-        }
-        else if(emotionSignal==2) {
-            ani.SetTrigger("joy");
-        }
-        else if(emotionSignal==3) {
-            ani.SetTrigger("fear");
-        }
-        else if(emotionSignal==4) {
-            ani.SetTrigger("disgust");
-        }
-        else if(emotionSignal==5) {
-            ani.SetTrigger("anger");
+        string message_flutter = messageInput.text;
+        if (message_flutter.Substring(1,1)==":") {
+            messageSignal = int.Parse(message_flutter.Substring(0,1));
+            message = message_flutter.Remove(0,2);
         }
         
         
