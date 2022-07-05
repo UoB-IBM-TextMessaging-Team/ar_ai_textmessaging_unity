a8a1fff381b254112c1f15f799fa70c896af5aff
Cheong43 <sameslam@outlook.com>
2022-07-05 12:08:53 +0100

    asemble file tracked issue fixed

--- android/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\AndroidProject\ar_ai_textmessaging\android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
--- android/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\AndroidProject\ar_ai_textmessaging\android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\BlobReader.cpp"
-#include "D:\AndroidProject\ar_ai_textmessaging\android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\Debugger.cpp"
-#include "D:\AndroidProject\ar_ai_textmessaging\android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "D:\AndroidProject\ar_ai_textmessaging\android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "D:\AndroidProject\ar_ai_textmessaging\android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\VmStringUtils.cpp"
-#include "D:\AndroidProject\ar_ai_textmessaging\android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System.Threading\Interlocked.cpp"
-#include "D:\AndroidProject\ar_ai_textmessaging\android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System\Math.cpp"
