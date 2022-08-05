985053fbe3a8297d2313b9196fa2bfdaece34815
Corey <13124729966@163.com>
2022-08-05 13:59:41 +0800

    add nluLanguage

--- unity/ARView-IBM-TextMessaging/Assets/Main_normal.cs
+++ unity/ARView-IBM-TextMessaging/Assets/Main_normal.cs
@@ -13,6 +13,7 @@ public class Main_normal : MonoBehaviour
 {
     [Header("AR Object To Place")]
     public GameObject objectToPlace;
+    public string nluLanguage = 'en';
 
     [Header("Message Input")]
     public TMP_InputField messageInput;
